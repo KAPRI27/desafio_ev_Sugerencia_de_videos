0d70e1ec9d1cdd9c801275a1e0e822a13ace102d
Paula_Maureira <paumau27@gmail.com>
2024-03-21 21:17:18 -0300

    revisando desafio

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+// IIFE (Expresión de Función Invocada Inmediatamente) que encapsula la lógica de inyectar y mostrar contenido
+let iifeGlobal = (() => {
+    let inyectarTodo = (url, id) => {
+      id.setAttribute("src", url);
+      id.style.display = "block";
+    };
+  
+    return {
+      mostrarTodo: (url, id) => inyectarTodo(url, id),
+    };
+  })();
+  
+  // Definición de la clase Multimedia para manejar la URL
+  class Multimedia {
+    constructor(url) {
+      let _url = url;
+  
+      this.getUrl = () => _url;
+    }
+  
+    // Getter para obtener la URL
+    get url(){
+        return this.getUrl;
+    };
+    // Método para establecer el inicio del video (no implementado)
+    
+  }
+  
+  // Definición de la clase Reproductor que hereda de Multimedia
+    class Reproductor extends Multimedia{
+        constructor (url){
+            super (url)
+        }
+    }      
+    // Método para establecer el inicio del video
+    setInicio(tiempo) {
+      // Modifica la URL del video para iniciar en un tiempo específico
+      this.getId().setAttribute("src", `${this.url}?start=${tiempo}`);
+    }
+  
+  
+  // Crear instancias de Reproductor para reproducir diferentes videos
+  let playMusica = new Reproductor(
+    let nuevoPlay = playMultimedia();
+    let setInicio = ();
+    musica;
+  );
+  playMusica.playMultimedia();
+  playMusica.setInicio(200);
+  
+  let playPelicula = new Reproductor(
+    "...",
+    peliculas
+  );
+  playPelicula.playMultimedia();
+  
+  let playSerie = new Reproductor(
+    "...",
+    series
+  );
+  playSerie.playMultimedia();
